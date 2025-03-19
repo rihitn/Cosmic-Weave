9e72a0618935589155da29dacf90b7b1d865d043
rihitn <rihitonitta@gmail.com>
2025-03-19 10:28:28 +0900

    文字化け対策リターンズ

--- backend/embedding.py
+++ backend/embedding.py
@@ -1,3 +1,6 @@
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
+
 import os
 import openai
 from supabase import create_client, Client
--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,6 @@
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
+
 import os
 import requests
 from bs4 import BeautifulSoup
