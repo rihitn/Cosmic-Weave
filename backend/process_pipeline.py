66e3385c3c39a635ad35e8fd0a498ee45ce363a8
rihitn <rihitonitta@gmail.com>
2025-03-20 20:44:08 +0900

    デプロイ用6

--- backend/process_pipeline.py
+++ backend/process_pipeline.py
@@ -69,4 +69,6 @@ def process_pipeline():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 8080))  # ← ここでPORTを環境変数から取得
+    app.run(host="0.0.0.0", port=port)
+
