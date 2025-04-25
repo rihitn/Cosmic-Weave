5c39cb36bd3be869dfae8994eb49da2ed7bf008e
rihitn <rihitonitta@gmail.com>
2025-04-26 02:05:49 +0900

    いろいろ

--- frontend/src/supabase.js
+++ frontend/src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const supabaseUrl = SUPABASE_URL; // ここに自分のURL
-const supabaseAnonKey = SUPABASE_KEY;
+const supabaseUrl = 	import.meta.env.VITE_SUPABASE_URL; // ここに自分のURL
+const supabaseAnonKey = 	import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
