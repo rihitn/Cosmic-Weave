94fa0873f3a0362974b5f78149736a2081ba13d5
rihitn <rihitonitta@gmail.com>
2025-04-26 00:48:17 +0900

    いろいろ

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'url';
+import path from 'path';
 
 export default defineConfig({
-  root: './public', // index.html がある場所
+  root: './public',
+  resolve: {
+    alias: {
+      '/src': path.resolve(__dirname, 'src'),
+    },
+  },
   build: {
     outDir: '../dist',
     emptyOutDir: true,
-  }
+  },
 });
+
